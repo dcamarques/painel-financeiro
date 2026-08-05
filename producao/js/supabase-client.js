4c8af00b186183e504d3a81d3d5c7233c167ee33
dcamarques <dcamarques@gmail.com>
2026-08-04 22:43:59 -0300

    Update supabase-client.js

--- producao/js/supabase-client.js
+++ producao/js/supabase-client.js
@@ -1,1 +1,6 @@
+// ATENÇÃO: Substitua os valores abaixo pelas suas credenciais reais do Supabase
+const SUPABASE_URL = 'https://vlnzhrhzzhbzdvhmsuap.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_fmqGGaxEPBatRTMzL5sTnw_FWrgvqr6';
 
+// Inicializa a ponte de comunicação entre seu GitHub Pages e o Supabase
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
